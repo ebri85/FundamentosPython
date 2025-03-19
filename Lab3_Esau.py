4034fe5941b0df1d778f89c958feeba1a9b60d34
Esau Brizuela <esau.brizuela@outlook.com>
2025-03-18 22:33:17 -0600

    Commit de las primeras dos funciones

--- Lab3_Esau.py
+++ Lab3_Esau.py
@@ -1,31 +1,35 @@
 """
 - Hacer una funcion que busque el numero mas pequeno en una lista, no se puede usar min ni ninguna funcion de Python
 
-- Funcion, de una lista de palabras, generar una lista de cantidad de palabras respectivamente
 """
 def num_menor():
-    numeros = [1,2,3,5,3,6,7,5,6,13,41,21,54,21]
+    numeros = [4,2,3,5,3,6,7,5,6,13,41,1,21,54,21]
     min = numeros[0]
     l = 0
-    for i in range(1,len(numeros)):
+    for i in numeros:
         if i < min:
-            min = numeros[i]
+            min = i
 
     return min
 
-print(num_menor())
+print("El numero menor de la lista es ",num_menor())
 """
 - Funcion para obtener la posicion del primer numero no repetido de una lista
 """
 
-def num_repetido():
-    numeros = [1,2,3,5,3,6,7,5,6,13,41,21,54,21]
-    repetido = numeros[0]
-    posicion =0
-    for i in range(1,len(numeros)):
-        if i == repetido:
-            posicion
+def num_no_repetido():
+    numeros = [1,2,3,2,1,5,3,6,7,5,6,13,41,21,54,21]
+    for i in range(len(numeros)):
+        cont =0
+        for j in range(len(numeros)):
+            if numeros[i]== numeros[j]:
+                cont += 1
+        if cont == 1:
+            return i
+ 
+ 
+print("La posicion del primer numero no repetido es ",num_no_repetido())
 
-    return repetido
 
-print(num_menor())
+"""- Funcion, de una lista de palabras, generar una lista de cantidad de palabras respectivamente
+"""
